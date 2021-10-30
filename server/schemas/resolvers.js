bc5eb04f600d1e6755672c460fa5d4cb6c4077b6
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-30 02:22:56 -0400

    Defined addUser Mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,6 +9,11 @@ const resolvers = {
     },
         
     Mutation: {
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
         login: async () => {
             const user = await User.findOne({
                 $or: [{ username: username }, { email: email }]
