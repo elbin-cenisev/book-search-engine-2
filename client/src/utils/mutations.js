5eff7e054e0ca90752029a2b7303250f4c0d1816
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-30 02:48:02 -0400

    Added REMOVE_BOOK mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,7 +25,7 @@ export const ADD_USER = gql`
 `;
 
 export const SAVE_BOOK = gql`
-  mutation saveBook(input: BookInput) {
+  mutation saveBook($input: BookInput) {
       saveBook(input: $input) {
           user {
               _id
@@ -40,4 +40,22 @@ export const SAVE_BOOK = gql`
           }
       }
   }
+`
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            user {
+                _id
+                username
+                email
+                savedBooks {
+                    bookId
+                    authors
+                    title
+                    description
+                }
+            }
+        }
+    }
 `
