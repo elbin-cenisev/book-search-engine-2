13c5af65290e045d9c0c4317a92c1bed3e3665bc
Elbin Cenisev <elbincenisev@outlook.com>
2021-10-30 02:20:04 -0400

    Resolved me Query

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,12 @@
 const resolvers = {
     Query: {
+        me: async (parent, {email, password}, context) => {
+            if(context.user) {
+                return User.findOne({ _id: context.user._id }).populate('savedBooks')
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        }
+    },
         
     }
 };
